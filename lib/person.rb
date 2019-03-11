dd0cb3f7badabc11dd2483d86166ca66506616de
Joan Lyness <joanrigdon@gmail.com>
2019-03-11 17:15:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,20 @@
-class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-  :weight, :handed, :complexion, :t_shirt_size,
-  :wrist_size, :glove_size, :pant_length, :pant_width
+# class Person
+#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+#   :weight, :handed, :complexion, :t_shirt_size,
+#   :wrist_size, :glove_size, :pant_length, :pant_width
+#
+#   def initialize(attributes)
+#     attributes.each {|key, value| self.send(("#{key}="), value)}
+#   end
+# end
 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+require 'pry'
+require 'Nokogiri'
+require 'open-uri'
+
+
+  def self.scrape_JPL
+    doc = Nokogiri::HTML(open("https://www.jpl.nasa.gov/missions/"))
+    binding.pry
+    url = doc.css("ul.articles li.slide a").first.text.strip
   end
-end
